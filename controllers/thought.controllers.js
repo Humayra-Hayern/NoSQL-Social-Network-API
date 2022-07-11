c34526764d4c891df696baf7055b4155fdd6033d
Humayra Hayern <hayern786@hotmail.com>
2022-07-11 13:31:03 +0100

    creating thought

--- controllers/thought.controllers.js
+++ controllers/thought.controllers.js
@@ -33,3 +33,24 @@ const thoughtController = {
       });
   },
 
+  //create thought
+  createThought({ params, body }, res) {
+    Thought.create(body)
+      .then(({ _id }) => {
+        return User.findOneAndUpdate(
+          { username: body.username },
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res
+            .status(404)
+            .json({ message: "No user found with this username!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
