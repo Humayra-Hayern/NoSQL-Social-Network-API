266bd59e41a3ef81253b9446e43aa7914d21310a
Humayra Hayern <hayern786@hotmail.com>
2022-07-15 00:31:21 +0100

    moved line 11 to line 22

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.use(require("./routes"));
-
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/social-network-API",
   {
@@ -19,5 +17,5 @@ mongoose.connect(
 
 // Use this to log mongo queries being executed!
 mongoose.set("debug", true);
-
+app.use(require("./routes"));
 app.listen(PORT, () => console.log(` Connected on localhost:${PORT}`));
